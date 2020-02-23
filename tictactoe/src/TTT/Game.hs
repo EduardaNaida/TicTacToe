969ce358fc00e5c37e2afd13e4e6b30a59f17cae
unknown <oliver.robert1999@gmail.com>
2020-02-23 19:05:38 +0100

    Built functions for editing and generating boards. Added some comments.

--- tictactoe/src/TTT/Game.hs
+++ tictactoe/src/TTT/Game.hs
@@ -4,8 +4,12 @@ module TTT.Game where
     import System.Random
     import Text.Read
     
+    type Point = (Int, Int)
+    
+    newtype Board = Board [[Slot]]
+
     data Slot = Empty | Full Player
-    data Player = X | O deriving(Show, Eq)
+    data Player = X | O deriving(Eq)
     
     instance Show Slot where
         show Empty = " "
@@ -16,11 +20,6 @@ module TTT.Game where
         show X = "X"
         show O = "O"
     
-    world = "\n" ++ show (slot 1) ++ "|"
-    
-    slot n = currentBoard!!(n-1)
-    
-    currentBoard = replicate 9 Empty
     
     --Turn
     
@@ -33,7 +32,7 @@ module TTT.Game where
         if (i == 1) then
             return X
         else
-            return X
+            return O
     
     
     playerInsert :: Player -> Slot
@@ -41,11 +40,38 @@ module TTT.Game where
     playerInsert O = Full O
     
     initialBoard :: Board
-    
+    initialBoard = Board replicate 3 (replicate 3 Empty)
+
+    {-
+    genBoard :: Int -> Board
+    genBoard int = Board replicate int (replicate int Empty)
+    -}
+
+    boardElem :: Board -> Point -> Slot
+    boardElem board point = undefined
+
     printBoard :: Board -> IO ()
+    printBoard = undefined
+
+    --coordinates (a,b) 
+    --a: horizontal row 
+    --b: pos. in row (1-3)
+    replaceBoard :: Board -> Point -> a -> Board
+    replaceBoard (Board list) point player = Board replaceList (list !! (fst point)) (snd point) player
+
+    --använd för att uppdatera en lista med ett nytt element 'insert'
+    replaceList :: [a] -> Int -> a -> [a]
+    replaceList list int insert = x ++ insert : ys
+        where (x,_:ys) = splitAt int list
     
     -- För att parsea moves, använd readMaybe från Text.Read
     
+    {- readMove
+        Checks if the standard input is a point 
+        Returns: The input Point
+        Side effect: Reads one or more lines from standard input 
+    -}
+
     readMove :: IO Point
     readMove = do
         str <- getLine
@@ -55,10 +81,15 @@ module TTT.Game where
                 putStrLn "Invalid input."
                 readMove
     
+    {- runGame
+        Starts the game with either X or O's turn
+        Returns: 
+    -}
+
     runGame :: IO ()
     runGame = do
         player <- startingPlayer
-        gameLoop player board
+        gameLoop movesLeft player initialBoard
     
     gameLoop :: Int -> Player -> Board -> IO ()
     gameLoop movesLeft turn board = do
@@ -68,7 +99,9 @@ module TTT.Game where
       point <- readMove
       -- använd makeMove för nytt bräde. Om invalid, kör samma gameLoop igen
       -- om valit, kolla vinst; om ingen vinst, kör gameLoop på det nya brädet, dekrementera movesLeft, och byt spelare
+      putStrLn "tja"
     
+
     --ger ut antingen 1 eller 2
     yesno :: IO Int
     yesno = do 
@@ -76,18 +109,10 @@ module TTT.Game where
         return a
     
     
-    type Point = (Int, Int)
-    
-    newtype Board = Board [[Slot]]
-    
     makeMove :: Point -> Board -> Player -> Maybe Board
+    makeMove point board player = undefined
     
     checkWin :: Point -> Board -> Maybe Player
+    checkWin = undefined
     
     -- Behöver kolla draws. Ett sätt är att bära omkring en räknare som dekrementeras med varje legalt drag; börjar på antalet spaces; oavgjort när den når 0 utan att nån har vunnit
-    
-    
-    --använd för att uppdatera en lista med ett nytt element 'insert'
-    replaceList :: [a] -> Int -> a -> [a]
-    replaceList list int insert = x ++ insert : ys
-        where (x,_:ys) = splitAt int list
